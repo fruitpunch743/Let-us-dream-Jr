3309daa03cb458f69fd4ac520e95acde733a01bb
Meenakshi S A <107346096+fruitpunch743@users.noreply.github.com>
2023-06-20 21:03:59 +0530

    Update index.php

--- dreams/mentee_home/index.php
+++ dreams/mentee_home/index.php
@@ -22,107 +22,9 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Document</title>
-  <link rel="stylesheet" href="index.css" />
-  
-  <style>
-    @import "compass/css3";
-
-@import url(https://fonts.googleapis.com/css?family=Montserrat:400,700);
-@import url(https://fonts.googleapis.com/css?family=Fjalla+One);
-
-.badge{
-  position: relative;
-  width: 416px;
-  display: inline-block;
-  margin-top: 80px;
-  margin-left: 0px;
-  margin-bottom: 50px;
-  .title{
-    font-family: "Montserrat", sans-serif;
-    font-weight: bold;
-    font-size: 2.0em;
-    position: absolute;
-    top: -28px;
-    border-radius: 8px 8px 8px 8px;
-    text-align: center;
-    width: 100%;
-    background:  #66ff99;
-    padding: 20px 30px;
-  }
-  
-}
-
-@mixin setColor($color){
-  .title{
-      background: #66ff99;
-  }
+  <link rel="stylesheet" href="inde.css" />
+  <link rel="stylesheet" href="badge.css" />
  
-}
-@import url(https://fonts.googleapis.com/css?family=Bitter:700|Droid+Serif:400|400italic);
- 
- .structure {
-	 width: 200px;
-	 float: left;
-   margin-top:0px;
-   
-}
- .badge-two {
-	 position: relative;
-
-	 width: 100px;
-	 height: 100px;
-	 border: 2px dashed #478266;
-	 border-radius: 100px;
-}
- .badge-two:after {
-	 position: absolute;
-	 top: -9px;
-	 left: -9px;
-	 display: block;
-	 width: 115px;
-	 height: 115px;
-	 content: '';
-	 border: 4px solid #665136;
-	 border-radius: 108px;
-}
- .badge-two:before {
-	 position: absolute;
-	 top: 4px;
-	 left: 4px;
-	
-	 display: block;
-	 width: 88px;
-	 height: 88px;
-	 content: '';
-	 background-color: #2c7d40;
-	 border-radius: 100px;
-}
- .badge-two h3 {
-	 position: absolute;
-   font-family: 'Bitter', serif;
-	 font-weight: 700;
-	 top: 5%;
-	 left: 7%;
-	 z-index: 7;
-	 width: 80px;
-	 margin-top: 1.2em;
-	 font-size: 1.5em;
-	 text-align: center;
-	 text-shadow: 1px 1px 0 #e9d2a3;
-	 color: #ffffff;
-	 -webkit-transform: rotate(-30deg);
-	 -ms-transform: rotate(-30deg);
-	 transform: rotate(-30deg);
-}
- .badge-two h3 span {
-	 position: absolute;
-	 top: 2.5em;
-	 right: 0;
-	 left: 0;
-	 display: inline-block;
-}
-
-  </style>
 </head>
 
 <body>
@@ -132,22 +34,20 @@
     <h2 class="logout">Logout</h2>
   </div>
   <div class="row">
-    <div class="head px-5 mx-5 ">
-    
-    
-    </div>
-  
+    <div class="head px-5 mx-5 "></div>
     <div class="col-2"></div>
-    <div class="col-5  ">
-    <div class="badge red">
-      <p class="title" style="color:black;"><?php echo $name; ?><br>Desire group christ school</p>
-    </div>
-    
-    <div class="structure">
-        <div class="badge-two">
-          <h3>Year 1</h3> 
+    <div class="col">
+    <div class="blah">
+        <div class="badge red">
+            <p class="title" style="color:black;"><?php echo $name; ?><br>Desire group christ school</p>
         </div>
-    </div> 
+        <div class="structure">
+          <div class="badge-two">
+            <h3>Year 1</h3> 
+          </div>
+        </div> 
+    </div>
+
       <div class="row py-3">
         <a href="../mentee_course_materials/index.html" class="items">
        
@@ -180,25 +80,12 @@
         </a>
       </div>
     </div>
-    <div class="col-5">
-      <div class="row">
-        <div class="col-3"></div>
-        <div class="col-9 anmnts">
-          <h1 class="anmnts-header ps-5">Announcements</h1>
-          <div class="row">
-            <div class="col"></div>
-            <!-- Announcements items -->
-            <div class="col-10 my-3">
-              <div class="row anmnts-item  my-3">
-                <h5>abc</h5>
-              </div>
-
-
-              <div class="col"></div>
-            </div>
-          </div>
+    <div class="col">
+    <div class="announcement">
+            <img class="newimage" src="icons/announcement.png" alt="" />
+            <h4>Announcements</h4>
+                <?php include('./get_announcement.php') ?>
         </div>
-      </div>
     </div>
 </body>
 
